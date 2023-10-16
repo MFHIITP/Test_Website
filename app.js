6ec6ad4bcf8038eca9435acf620a4e938b61e6db
MFHIITP <118089487+MFHIITP@users.noreply.github.com>
2023-10-17 01:26:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,54 +14,55 @@ app.set('view engine', 'html');
 app.get("/", (req,res)=>{
     res.status(200).sendFile(__dirname + '/home.html');
 });
-app.post('/', (req, res)=>{
-    let name = req.body.name;
-    let number = req.body.age;
-    let email = req.body.email;
-    let address = req.body.address;
+// app.post('/', (req, res)=>{
+//     let name = req.body.name;
+//     let number = req.body.age;
+//     let email = req.body.email;
+//     let address = req.body.address;
     
-    let output = `The name is ${name}
-    The age is ${number}
-    The email is ${email}
-    The addrsss is ${address}`;
+//     let output = `The name is ${name}
+//     The age is ${number}
+//     The email is ${email}
+//     The addrsss is ${address}`;
 
-    fs.writeFileSync("output.txt", output);
-    res.send("The data is saved");
+//     fs.writeFileSync("output.txt", output);
+//     res.send("The data is saved");
+// });
+
+
+const server = "mongodb+srv://hossainfarshid:JUITfh-891@clusterfarshid.vcl5snh.mongodb.net/";
+const database = "Test_Database";
+
+const mongoose = require("mongoose");
+const connect = async()=>{
+    try{
+        await mongoose.connect(`mongodb://${server}/${database}`);
+        console.log("Connection successful");
+    }
+    catch(err){
+        console.log("Connection Unsuccessful");
+    }
+}
+connect();
+
+var schema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String,
+    address: String
 });
+
+var Collection1 = mongoose.model("Collection 1", schema);
+
+app.post('/', (req, res)=>{
+    var data = new Collection1(req.body);
+    data.save().then(()=>{
+        res.status(200).sendFile(__dirname + '/home.html');
+    }).catch(()=>{
+        res.send("The data has not been send");
+    });
+});
+
 app.listen(port, ()=>{
     console.log(`http://127.0.0.1:${port}`);
 });
-
-
-// const server = "127.0.0.1:27017";
-// const database = "Test_Database";
-
-// const mongoose = require("mongoose");
-// const connect = async()=>{
-//     try{
-//         await mongoose.connect(`mongodb://${server}/${database}`);
-//         console.log("Connection successful");
-//     }
-//     catch(err){
-//         console.log("Connection Unsuccessful");
-//     }
-// }
-// connect();
-
-// var schema = new mongoose.Schema({
-//     name: String,
-//     age: Number,
-//     email: String,
-//     address: String
-// });
-
-// var Collection1 = mongoose.model("Collection 1", schema);
-
-// app.post('/', (req, res)=>{
-//     var data = new Collection1(req.body);
-//     data.save().then(()=>{
-//         res.status(200).sendFile(__dirname + '/home.html');
-//     }).catch(()=>{
-//         res.send("The data has not been send");
-//     });
-// });
