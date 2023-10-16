7c6391f5764cb47daed6746fe8a5fe4c2e75c665
MFHIITP <118089487+MFHIITP@users.noreply.github.com>
2023-10-17 01:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,50 +18,50 @@ app.listen(port, ()=>{
     console.log(`http://127.0.0.1:${port}`);
 });
 
-// app.post('/', (req, res)=>{
-//     let name = req.body.name;
-//     let number = req.body.age;
-//     let email = req.body.email;
-//     let address = req.body.address;
+app.post('/', (req, res)=>{
+    let name = req.body.name;
+    let number = req.body.age;
+    let email = req.body.email;
+    let address = req.body.address;
     
-//     let output = `The name is ${name}
-//     The age is ${number}
-//     The email is ${email}
-//     The addrsss is ${address}`;
+    let output = `The name is ${name}
+    The age is ${number}
+    The email is ${email}
+    The addrsss is ${address}`;
 
-//     fs.writeFileSync("output.txt", output);
-//     res.status(200).end(home);
+    fs.writeFileSync("output.txt", output);
+    res.status(200).sendFile(__dirname + '/home.html');
+});
+
+// const server = "127.0.0.1:27017";
+// const database = "Test_Database";
+
+// const mongoose = require("mongoose");
+// const connect = async()=>{
+//     try{
+//         await mongoose.connect(`mongodb://${server}/${database}`);
+//         console.log("Connection successful");
+//     }
+//     catch(err){
+//         console.log("Connection Unsuccessful");
+//     }
+// }
+// connect();
+
+// var schema = new mongoose.Schema({
+//     name: String,
+//     age: Number,
+//     email: String,
+//     address: String
 // });
 
-const server = "127.0.0.1:27017";
-const database = "Test_Database";
+// var Collection1 = mongoose.model("Collection 1", schema);
 
-const mongoose = require("mongoose");
-const connect = async()=>{
-    try{
-        await mongoose.connect(`mongodb://${server}/${database}`);
-        console.log("Connection successful");
-    }
-    catch(err){
-        console.log("Connection Unsuccessful");
-    }
-}
-connect();
-
-var schema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    email: String,
-    address: String
-});
-
-var Collection1 = mongoose.model("Collection 1", schema);
-
-app.post('/', (req, res)=>{
-    var data = new Collection1(req.body);
-    data.save().then(()=>{
-        res.status(200).sendFile(__dirname + '/home.html');
-    }).catch(()=>{
-        res.send("The data has not been send");
-    });
-});
+// app.post('/', (req, res)=>{
+//     var data = new Collection1(req.body);
+//     data.save().then(()=>{
+//         res.status(200).sendFile(__dirname + '/home.html');
+//     }).catch(()=>{
+//         res.send("The data has not been send");
+//     });
+// });
